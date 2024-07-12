b388d4951af54a3195a245738c0fec12a8023fd1
ishans619 <ishanshukla619@gmail.com>
2024-07-12 19:28:21 +0530

    backend code for a basic todoList app using Spring Boot invloving basic CRUD operations!!

--- TodoRepo.java
+++ TodoRepo.java
@@ -1,5 +1,6 @@
 import com.Shukla.mytodolist.model.TodoItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//Jpa repository uses Hibernate ORM internally
 public interface TodoRepo extends JpaRepository<TodoItem, Long> {
 }
